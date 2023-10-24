4232e12023bc349e3c6d783fbd20293d71a2595a
IgnRL <nacho.romerolozano@gmail.com>
2023-10-24 13:25:46 +0200

    Completed Friedman's test and created plot. Started with Dunn's test (probably not worth it)

--- dirty_script_IRO.R
+++ dirty_script_IRO.R
@@ -1,43 +1,19 @@
-install.packages("datarium")
-library(datarium)
-selfsteem <- data("selfesteem", package = "datarium")
-head(selfesteem, 3)
+ibrary(ggplot2)
+library(gridExtra)
+library(tidyr)
+
+actualIris <- data.frame(Sepal.Length=6.1, Sepal.Width=3.1, Petal.Length=5.0, Petal.Width=1.7)
 
 
-# Gather columns t1, t2 and t3 into long format
-# Convert id and time into factor variables
-selfesteem2 <- selfesteem %>%
-  gather(key = "time", value = "score", t1, t2, t3) %>%
-  convert_as_factor(id, time)
-head(selfesteem2, 3)
+labels <- c(paste('Petal Length\none-tailed test=', round(sum(actualIris$Sepal.Length < iris$Sepal.Length)/nrow(iris), 2)),
+            paste('Petal Width\none-tailed test=', round(sum(actualIris$Sepal.Width < iris$Sepal.Width)/nrow(iris), 2)),
+            paste('Sepal Length\none-tailed test=', round(sum(actualIris$Petal.Length < iris$Petal.Length)/nrow(iris), 2)),
+            paste('Sepal Width\none-tailed test=', round(sum(actualIris$Petal.Width < iris$Petal.Width)/nrow(iris), 2)))
 
-
-
-banana.paleta = list(
-  paleta.treat = c("red", "blue", "yellow3", "green4",
-                  "red4", "skyblue", "gold"),
-  my_favourite_colours = c("#702963", "#637029",    "#296370")
-)
-
-banana.paleta.f = function(name, n, all_palettes = banana.paleta, type = c("discrete", "continuous")) {
-  palette = all_palettes[[name]]
-  if (missing(n)) {
-    n = length(palette)
-  }
-  type = match.arg(type)
-  out = switch(type,
-               continuous = grDevices::colorRampPalette(palette)(n),
-               discrete = palette[1:n] )
-  structure(out, name = name, class = "palette")
-}
-banana.paleta.f("paleta.treat", all_palettes = banana.paleta, type= "discrete")
-
-scale_colour_banana_disc = function(name) {
-  ggplot2::scale_colour_manual(values = banana.paleta.f(name, type = "discrete"))
-}
-
-scale_fill_banana_disc = function(name) {
-  ggplot2::scale_fill_manual(values = banana.paleta.f(name,
-                                                     type = "discrete"))
-}
-
+tmp <- iris[,-5] %>% gather(Type, value)
+actuals <- data.frame(col1=colnames(actualIris), col2=as.numeric(actualIris[1,]))
+tmp$Actual <- actuals$col2[match(tmp$Type, actuals$col1)]
+tmp$Type <- factor(tmp$Type, levels = c('Petal.Length', 'Petal.Width', 'Sepal.Length', 'Sepal.Width'), 
+                   labels = labels)
+ggplot(tmp, aes(value)) + facet_wrap(~Type, scales="free", nrow = 1) + geom_histogram() + 
+  geom_vline(aes(xintercept=Actual), colour="blue", lwd=2) 
