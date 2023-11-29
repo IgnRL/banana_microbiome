29c2cd9d70a1fc460051787bdda61744b2180015
IgnRL <nacho.romerolozano@gmail.com>
2023-11-29 16:53:18 +0100

    Added delta graphics for diversity index and did minor modifications to PCA plots

--- dirty_script_IRO.R
+++ dirty_script_IRO.R
@@ -1,37 +1,37 @@
 zotu.rel<-  transform_sample_counts(zotu97, function(x) x / sum(x))
 #SUBSET BY TIME
 zotu.rel.t0<- subset_samples(zotu.rel, time== "T0")
-zotu.rel.t4<- subset_samples(zotu.rel, time== "T4")
-zotu.rel.t0.t4 <- subset_samples(zotu.rel, time =="T0" | time =="T4")
+zotu.rel.t5<- subset_samples(zotu.rel, time== "T5")
+zotu.rel.t0.t5 <- subset_samples(zotu.rel, time =="T0" | time =="T5")
 
-##### T0 + T4
-t.otu <- zotu.rel.t0.t4 %>% otu_table() %>% data.frame() %>% t()
+##### T0 + T5
+t.otu <- zotu.rel.t0.t5 %>% otu_table() %>% data.frame() %>% t()
 t.otu.clr <- decostand(t.otu, method = "clr", pseudocount = 1)
 pca.clr <- prcomp(t.otu.clr, scale=TRUE)
 scores <- data.frame(pca.clr$x)
-t0.t4.pca <- data.frame(scores, sample_data(zotu.rel.t0.t4))
+t0.t5.pca <- data.frame(scores, sample_data(zotu.rel.t0.t5))
 variance_perc <- pca.clr$sdev^2 / sum(pca.clr$sdev^2) * 100 
 
-pca.plot.t0.t4.treat <- ggplot(t0.t4.pca, aes(PC1, PC2, col= treatment, shape= time))+
+pca.plot.t0.t5.treat <- ggplot(t0.t5.pca, aes(PC1, PC2, col= treatment, shape= time))+
   geom_point(size= 3)+
   scale_color_manual(values= treat.colors)+
   scale_shape_manual(values=c(1,16))+
-  labs(title= "T0 and T4 CLR transformed",
-       x= sprintf("PC1 (%.1f%%)", variance_explained[1]), 
-       y= sprintf("PC2 (%.1f%%)", variance_explained[2]))+
+  labs(title= "T0 and T5 CLR transformed",
+       x= sprintf("PC1 (%.1f%%)", variance_perc[1]), 
+       y= sprintf("PC2 (%.1f%%)", variance_perc[2]))+
   stat_ellipse(aes(group =treatment, fill= treatment), level = 0.95, geom = "polygon", alpha = 0.2)+
   scale_fill_manual(values= treat.colors)
-pca.plot.t0.t4.treat
+pca.plot.t0.t5.treat
 
-pca.plot.t0.t4.time <- ggplot(t0.t4.pca, aes(PC1, PC2, col= treatment, shape= time))+
+pca.plot.t0.t5.time <- ggplot(t0.t5.pca, aes(PC1, PC2, col= treatment, shape= time))+
   geom_point(size= 3)+
   scale_color_manual(values= treat.colors)+
   scale_shape_manual(values=c(1,16))+
-  labs(title= "T0 and T4 CLR transformed",
-       x= sprintf("PC1 (%.1f%%)", variance_explained[1]), 
-       y= sprintf("PC2 (%.1f%%)", variance_explained[2]))+
+  labs(title= "T0 and T5 CLR transformed",
+       x= sprintf("PC1 (%.1f%%)", variance_perc[1]), 
+       y= sprintf("PC2 (%.1f%%)", variance_perc[2]))+
   stat_ellipse(aes(group =time, fill= time), level = 0.95, geom = "polygon", alpha = 0.2)
-pca.plot.t0.t4.time
+pca.plot.t0.t5.time
 
 
 ##### T0 
@@ -46,29 +46,35 @@ pca.plot.t0 <- ggplot(t0.pca, aes(PC1, PC2, col= treatment))+
   geom_point(size= 3, shape= 1)+
   scale_color_manual(values= treat.colors)+
   labs(title= "T0 CLR transformed",
-       x= sprintf("PC1 (%.1f%%)", variance_explained[1]), 
-       y= sprintf("PC2 (%.1f%%)", variance_explained[2]))+
+       x= sprintf("PC1 (%.1f%%)", variance_perc[1]), 
+       y= sprintf("PC2 (%.1f%%)", variance_perc[2]))+
   geom_polygon(aes(fill =  treatment, group = interaction(time, treatment)),alpha= 0.5)+
   scale_fill_manual(values= treat.colors)
 pca.plot.t0
 
 
-##### T4
-t.otu <- zotu.rel.t4 %>% otu_table() %>% data.frame() %>% t()
+##### T5
+t.otu <- zotu.rel.t5 %>% otu_table() %>% data.frame() %>% t()
 t.otu.clr <- decostand(t.otu, method = "clr", pseudocount = 1)
 pca.clr <- prcomp(t.otu.clr, scale=TRUE)
 scores <- data.frame(pca.clr$x)
-t4.pca <- data.frame(scores, sample_data(zotu.rel.t4))
+t5.pca <- data.frame(scores, sample_data(zotu.rel.t5))
 variance_perc <- pca.clr$sdev^2 / sum(pca.clr$sdev^2) * 100 
 
-pca.plot.t4 <- ggplot(t4.pca, aes(PC1, PC2, col= treatment))+
+pca.plot.t5 <- ggplot(t5.pca, aes(PC1, PC2, col= treatment))+
   geom_point(size= 3, shape= 16)+
   scale_color_manual(values= treat.colors)+
-  labs(title= "T4 CLR transformed",
-       x= sprintf("PC1 (%.1f%%)", variance_explained[1]), 
-       y= sprintf("PC2 (%.1f%%)", variance_explained[2]))+
+  labs(title= "T5 CLR transformed",
+       x= sprintf("PC1 (%.1f%%)", variance_perc[1]), 
+       y= sprintf("PC2 (%.1f%%)", variance_perc[2]))+
   geom_polygon(aes(fill =  treatment, group = interaction(time, treatment)),alpha= 0.5)+
   scale_fill_manual(values= treat.colors)
-pca.plot.t4
+pca.plot.t5
 
-pca.multiplot <- grid.arrange(pca.plot.t0, pca.plot.t4, pca.plot.t0.t4.treat,pca.plot.t0.t4.time , layout_matrix= rbind(c(1,2), c(3,3), c(4,4)))
+pca.multiplot <- grid.arrange(pca.plot.t0 + guides(fill= "none", col= "none"), 
+                              pca.plot.t5+ guides(fill= "none", col= "none"),
+                              pca.plot.t0.t5.treat+ guides(fill= "none", col= "none")
+                              ,pca.plot.t0.t5.time+guides(fill= "none", alpha = "none")+theme_classic()+theme(legend.position = "bottom"),
+                              layout_matrix= rbind(c(1,2), c(3,3), c(4,4)))
+
+ggsave(pca.multiplot, file= "ITS_results/PCA/pca_all.png")
